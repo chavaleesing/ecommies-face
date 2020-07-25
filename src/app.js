607eac0cc44bfd3c7e2bbad977364a97835a7288
chavaleesing <chavalee.sing@gmail.com>
2020-07-25 16:13:18 +0700

    update main app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
 import Header from './components/header'
+import Sidebar from './components/sidebar'
 import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
@@ -13,16 +14,24 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <React.Fragment>
-        <Header />
-        <Switch>
-          <Route exact path={'/'} render={() => {
-            return <Redirect to={'/home'} />
-          }} />
-          <Route path="/home" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/productdetail" component={Detail} />
-        </Switch>
+        <div className="row-sm">
+          <Header />
+        </div>
+        <div className="row">
+          <div className="col-2">
+            {/* <p>PPPP</p> */}
+            <Sidebar />
+          </div>
+          <div className="col-10">
+            <Switch>
+              <Route exact path={'/'} render={() => { return <Redirect to={'/home'} /> }} />
+              <Route path="/home" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/productdetail" component={Detail} />
+            </Switch>
+          </div>
+        </div>
       </React.Fragment>
     </BrowserRouter>
   )
