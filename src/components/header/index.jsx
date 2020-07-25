140c47966a9580c5e1ff2cd1ab5b552aa55b24e3
chavaleesing <chavalee.sing@gmail.com>
2020-07-25 16:12:37 +0700

    update header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,15 +4,39 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div className="container">
-                <NavLink className="navbar-brand m-4" to="/">ECOMMIES</NavLink>
-                <input class="form-control mr-sm-2" type="search" placeholder="Search ..." aria-label="Search" />
-                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-                <NavLink className="navbar-brand ml-4" to="/login">
-                    Login <i class="fa fa-user-circle-o" style={{ color: "white" }} aria-hidden="true"></i>
-                </NavLink>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Ecommies</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Filter
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">111</a>
+                            <a class="dropdown-item" href="#">222</a>
+                            <a class="dropdown-item" href="#">333</a>
+                        </div>
+                    </li>
+                </ul>
             </div>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+
         </nav>
     );
 };
