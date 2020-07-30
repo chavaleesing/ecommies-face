44a871064b5cfba160b8439551b76aa755820189
chavaleesing <chavalee.sing@gmail.com>
2020-07-30 21:02:43 +0700

    update header comp

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
+import './style.scss';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Ecommies</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Filter
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">111</a>
-                            <a class="dropdown-item" href="#">222</a>
-                            <a class="dropdown-item" href="#">333</a>
-                        </div>
-                    </li>
-                </ul>
+        <nav className="navbar navbar-hd">
+            <div className="container">
+                <div className="col-2">
+                    <a className="navbar-brand" href="http://localhost:3000/home">Más FOODEST</a>
+                </div>
+                <div className="col-6">
+                    <input className="form-control my-5" type="search" placeholder="Search .." aria-label="Search" />
+                </div>
+                <div className="col-2">
+                    <button className="btn btn-primary w-100" type="submit">Search</button>
+                </div>
+                <div className="col-2">
+                    <a className="nav-item" href="http://localhost:3000/login">Login/ Register</a>
+                </div>
             </div>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-
         </nav>
     );
 };
