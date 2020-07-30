3e33cd388474bda7c877a6d59f719d7121f7cd95
chavaleesing <chavalee.sing@gmail.com>
2020-07-30 23:30:48 +0700

    update page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,15 +5,25 @@ import Sidebar from '../../components/sidebar'
 
 const Home = () => {
     const products = [
-        { name: "item1" }, { name: "item2" }, { name: "item3" }, { name: "item4" }, { name: "item5" }, { name: "item6" },
-        { name: "item7" }, { name: "item8" }, { name: "item9" }, { name: "item10" }, { name: "item11" }, { name: "item12" }
+        { name: "item1", "price": 1 }, 
+        { name: "item2", "price": 2 }, 
+        { name: "item3", "price": 3 }, 
+        { name: "item4", "price": 4 }, 
+        { name: "item5", "price": 5 }, 
+        { name: "item6", "price": 6 },
+        { name: "item7", "price": 7 }, 
+        { name: "item8", "price": 8 }, 
+        { name: "item9", "price": 9 }, 
+        { name: "item10", "price": 10 }, 
+        { name: "item11", "price": 11 }, 
+        { name: "item12", "price": 12 }
     ]
     return (
         <React.Fragment>
             <Sidebar />
             <div className="container">
                 <div className="row mt-5 justify-content-center">
-                    {products.map(product => <Product name={product.name} />)}
+                    {products.map(product => <Product product={product}/>)}
                 </div>
             </div>
         </React.Fragment>
--- src/pages/productDetail/index.jsx
+++ src/pages/productDetail/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ProductDetail from '../../components/productDetail'
+import Sidebar from '../../components/sidebar'
 
 
 const Detail = () => {
+    const product = { 
+        name: "item1", 
+        price: 1, 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fuga facilis possimus est deleniti, aliquid quam impedit. Voluptatum nesciunt, et enim facere est dicta repudiandae ipsum excepturi delectus, quasi deleniti." 
+    }
+    
     return (
         <React.Fragment>
-            <ProductDetail />
+            <Sidebar />
+            <ProductDetail product={product}/>
         </React.Fragment>
     );
 };
