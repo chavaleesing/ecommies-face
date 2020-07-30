492cc6999148f4599f33cc50260234575aa2712f
chavaleesing <chavalee.sing@gmail.com>
2020-07-30 21:02:54 +0700

    update sidebar comp

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './style.scss';
 
 const Sidebar = () => {
 
     return (
-        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-            <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-            <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
-        </div>
+        <nav class="nav nav-hm">
+            <div className="container">
+                <div className="row row-hm justify-content-center">
+                    <a class="nav-link m-3" href="#">All</a>
+                    <a class="nav-link m-3" href="#">Cookie</a>
+                    <a class="nav-link m-3" href="#">Brownie</a>
+                    <a class="nav-link m-3" href="#">SoftCake</a>
+                    <a class="nav-link m-3" href="#">Cupcake</a>
+                    <a class="nav-link m-3" href="#">PanCake</a>
+                </div>
+            </div>
+        </nav>
     );
 };
 
