023c183d10df03e392e32cbeea14047ce67aa7f5
chavaleesing <chavalee.sing@gmail.com>
2020-08-12 14:08:13 +0700

    update comment component

--- src/components/comment/index.jsx
+++ src/components/comment/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Component } from 'react';
 import './style.scss';
 import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
-import Ratings from '../rating' 
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import Ratings from '../rating'
 
 class Comment extends Component {
 
@@ -17,11 +19,15 @@ class Comment extends Component {
                 <div className="comment-header">
                     <AccountCircleSharpIcon />
                     <p className="comment-username"> User2910</p>
-                    <Ratings rate={5}/>
+                    <Ratings rate={5} />
                     <p className="comment-date-created"> 11/01/2020</p>
                 </div>
                 <div className="comment-body">
                     <p>This is the best cookie I ever had, most valuable</p>
+                </div>
+                <div className="comment-footer">
+                    <button type="button" class="btn btn-light thumb-up"><ThumbUpIcon/>(20)</button>
+                    <button type="button" class="btn btn-light thumb-down"><ThumbDownIcon />(7)</button>
                 </div>
             </div>
         );
