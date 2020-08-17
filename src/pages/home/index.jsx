0757e1af9eb90e54b7ffc5d613c6050112f22a0e
chavaleesing <chavalee.sing@gmail.com>
2020-08-17 21:16:04 +0700

    modify mock data for display

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,18 +5,18 @@ import Sidebar from '../../components/sidebar'
 
 const Home = () => {
     const products = [
-        { name: "item1", "price": 1 }, 
-        { name: "item2", "price": 2 }, 
-        { name: "item3", "price": 3 }, 
-        { name: "item4", "price": 4 }, 
-        { name: "item5", "price": 5 }, 
-        { name: "item6", "price": 6 },
-        { name: "item7", "price": 7 }, 
-        { name: "item8", "price": 8 }, 
-        { name: "item9", "price": 9 }, 
-        { name: "item10", "price": 10 }, 
-        { name: "item11", "price": 11 }, 
-        { name: "item12", "price": 12 }
+        { name: "item1", "price": 1, "rate": 4 }, 
+        { name: "item2", "price": 2, "rate": 2.5 }, 
+        { name: "item3", "price": 3, "rate": 0 }, 
+        { name: "item4", "price": 4, "rate": 1 }, 
+        { name: "item5", "price": 5, "rate": 3.5 }, 
+        { name: "item6", "price": 6, "rate": 2.5 },
+        { name: "item7", "price": 7, "rate": 5 }, 
+        { name: "item8", "price": 8, "rate": 5 }, 
+        { name: "item9", "price": 9, "rate": 4 }, 
+        { name: "item10", "price": 10, "rate": 4.5 }, 
+        { name: "item11", "price": 11, "rate": 4 }, 
+        { name: "item12", "price": 12, "rate": 3 }
     ]
     return (
         <React.Fragment>
