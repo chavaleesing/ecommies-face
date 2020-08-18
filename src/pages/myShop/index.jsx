bd87177eab38d1e247ad01f159f0f2bc72adfa7c
chavaleesing <chavalee.sing@gmail.com>
2020-08-18 16:41:52 +0700

    update myshop page

--- src/pages/myShop/index.jsx
+++ src/pages/myShop/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Product from '../../components/product'
 import Sidebar from '../../components/sidebar'
 import './style.scss';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Icon from '@material-ui/core/Icon';
 
 
 const MyShop = () => {
     const products = [
-        { name: "item1", "price": 1, "rate": 4 }, 
-        { name: "item2", "price": 2, "rate": 2.5 }, 
+        { name: "item1", "price": 1, "rate": 4 },
+        { name: "item2", "price": 2, "rate": 2.5 },
+        { name: "item1", "price": 1, "rate": 4 },
+        { name: "item2", "price": 2, "rate": 2.5 },
         { name: "item3", "price": 3, "rate": 0 }
     ]
     return (
@@ -17,11 +19,10 @@ const MyShop = () => {
             <div className="container">
                 <div className="myshop-header">
                     <h3 >My Items</h3>
-                    <AddCircleIcon className="add-btn"/>
+                    <a className="btn btn-info" href="http://localhost:3000/newitem">New Item</a>
                 </div>
-                {/* <AddCircleIcon className="add-btn"/> */}
                 <div className="row mt-5 justify-content-center">
-                    {products.map(product => <Product product={product}/>)}
+                    {products.map(product => <Product product={product} />)}
                 </div>
             </div>
         </React.Fragment>
